87972c592aaa56c790c1464f20df87db287ee3ac
jhoel sv <jhoel.sv25@gmail.com>
2023-09-01 16:32:05 -0500

    update cv download

--- src/app/modules/portfolio/components/home-page/home-page.component.ts
+++ src/app/modules/portfolio/components/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, inject } from '@angular/core';
 import { RedesSocialsService } from '../../services/redes-socials.service';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'home-page',
@@ -8,10 +9,18 @@ import { RedesSocialsService } from '../../services/redes-socials.service';
 })
 export class HomePageComponent {
   private  socialService = inject(RedesSocialsService)
-  public socials = computed(()=>this.socialService.allSocials)
+  public socials = computed(()=>this.socialService.allSocials);
+  private readonly Url_CV = environment.UrlCV
 
 
-  download(){
+  downloadPDF(){
+    const url = this.Url_CV;
+    const link : HTMLAnchorElement = document.createElement('a');
+    link.href = url;
+    link.setAttribute('downloasd', ' cv.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link)
 
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production:true,
+    UrlCV :'https://drive.google.com/u/0/uc?id=1RRmEBC7f7PrJ5iRxHWLTdAuPEaCr7ed7&export=download'
+};
